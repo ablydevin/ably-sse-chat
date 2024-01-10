f520fb470c58cf2a32321697cf0c1d051b22fdbd
ablydevin <136481955+ablydevin@users.noreply.github.com>
2024-01-10 10:25:05 -0500

    Initial Commit

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,50 +1,80 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { useEventSource } from "react-use-websocket";
 //import Message from "./Message";
 
+let lastEventId;
+
 export default function Messages() {
+  const [eventSourceUrl, setEventSourceUrl] = useState(null);
   const [messageEnvelopes, setMessageEnvelopes] = useState([]);
 
-  function getRealtimeData(messageEnvelope) {
+  const getRealtimeData = (messageEnvelope) => {
     console.log(messageEnvelope);
+    lastEventId = messageEnvelope.id
     setMessageEnvelopes([...messageEnvelopes, messageEnvelope]);
   }
 
-  const receiveMessages = () => {
-    fetch("/api/ably/").then((response) => {
-      response.json().then((accessToken) => {
-
-        console.log(accessToken.token);
-        const url = `https://realtime.ably.io/event-stream?channels=chat-publish&v=1.2&accessToken=${accessToken.token}`;
-
-        const sse = new EventSource(url);
-        sse.onmessage = (e) => getRealtimeData(JSON.parse(e.data));
-        sse.onerror = (e) => {
-          // error log here
-          console.log(e);
-          sse.close();
-        };
-        return () => {
-          sse.close();
-        };
-
-      });
-    });
-  };
+  const lastEventParam = lastEventId ? (`&lastEvent=${lastEventId}`) : '';
 
   useEffect(() => {
-    receiveMessages();
+    fetch("/api/ably/").then((response) => {
+      response.json().then((accessToken) => {
+        setEventSourceUrl(`https://realtime.ably.io/event-stream?channels=chat-publish${lastEventParam}&v=1.2&accessToken=${accessToken.token}`);
+      });
+    });
   }, []);
+
+  const { lastEvent, getEventSource, readyState } = useEventSource(
+    eventSourceUrl, 
+    {
+      retryOnError: true,
+      events: {
+        message: (messageEvent) => {
+          console.log('This has type "message": ', messageEvent);      
+          getRealtimeData(JSON.parse(messageEvent.data));
+        },
+        update: (messageEvent) => {
+          console.log('This has type "update": ', messageEvent);
+        }
+      }
+    }
+  );
+
+  // const receiveMessages = () => {
+  //   fetch("/api/ably/").then((response) => {
+  //     response.json().then((accessToken) => {
+
+  //       console.log(accessToken.token);
+  //       const url = `https://realtime.ably.io/event-stream?channels=chat-publish${lastEventParam}&v=1.2&accessToken=${accessToken.token}`;
+
+  //       const sse = new EventSource(url);
+  //       sse.onmessage = (e) => getRealtimeData(JSON.parse(e.data));
+  //       sse.onerror = (e) => {
+  //         // error log here
+  //         console.log(e);
+  //         sse.close();
+  //       };
+  //       return () => {
+  //         sse.close();
+  //       };
+
+  //     });
+  //   });
+  // };
+
+  // useEffect(() => {
+  //   receiveMessages();
+  // }, []);
 
   return (
     <div>
       <p>Messages</p>
       <ul>
-        {
-        messageEnvelopes.map((m) => 
+        {messageEnvelopes.map((m) => (
           <li key={m.id}>{JSON.parse(m.data).messageContent}</li>
-        )}
+        ))}
       </ul>
     </div>
   );
